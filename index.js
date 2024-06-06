043ef025e2ef270c879e0bd38a049796a248eea3
AMIRKIAYI <amirkiayi516@gmail.com>
2024-06-06 16:36:07 +0300

    index.js file update

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+document.querySelector("form").addEventListener("submit", postMovie)
+
+
+    function postMovie(e){
+        e.preventDefault()
+
+        let title = document.getElementById("title")
+let plot = document.getElementById("plot")
+let poster = document.getElementById("poster")
+
+const objectmovie ={
+    Title : title.value,
+    Plot : plot.value,
+    Poster : poster.value,
+}
+
+        fetch("http://localhost:3000/movies",{
+            method:'POST',
+            header:{
+                'content-Type':'application/json'
+            },
+            body:JSON.stringify(objectmovie)
+        })
+        //.then(response => response.json())
+       // .then(console.log("submitted"))
+    }
 
 
 
@@ -7,22 +33,41 @@ function addmovie(movie){
     let div = document.createElement("div")
     div.classList.add("col-3")
     div.innerHTML = `<div class="card">
-    <img src="${movie.Poster}" class="card-img-top" alt="...">
+    <img src="${movie.Poster}" class="card-img-top" alt="..." height="300px">
     <div class="card-body">
       <h5 class="card-title hind-bold ">${movie.Title}</h5>
-      <p class="card-text hind-light ">${movie.Plot}.</p>
+      <p class="card-text hind-light">${movie.Plot}.</p>
       <a href="#" class="btn btn-primary">Watch</a>
+      <button class="btn bg-danger"><i class="fa fa-trash"></i> Trash</button>
     </div>
   </div>`
   
 
   row.appendChild(div)
- 
- 
+
+  div.querySelector("button").addEventListener("click", function(){
+    
+
+    deleteData(movie.id)
 
 
 
+  })
 }
+
+  function deleteData(id){
+   
+    fetch(`http://localhost:3000/movies/${id}`, {
+            method:'DELETE',
+            header:{
+                'content-Type':'application/json'
+            }
+        })
+        .then(response => response.json())
+
+  }
+
+
 
 
 function getContent(movies){
@@ -46,3 +91,5 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
 })
+
+
